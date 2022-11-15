5a961ea2c764badd3276867c355b463ba5175d52
Biel Rabasa <bielrabasa@gmail.com>
2022-11-15 17:04:49 +0100

    benchSitting

---
+++ AI_Lab/Assets/_StateMachines/GoBenchState.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GoBenchState : StateMachineBehaviour
+{
+    Movement movement;
+
+    Vector3 currentBench;
+    Vector3[] benchLocations = { 
+        new Vector3(23, 0, -47), 
+        new Vector3(-16, 0, -47), 
+        new Vector3(-49, 0, -21), 
+        new Vector3(-49, 0, 23) 
+    };
+
+    //OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
+    override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        //animator.ResetTrigger("goBench");
+        movement = animator.GetComponent<Movement>();
+
+        currentBench = benchLocations[Random.Range(0, 4)];
+        movement.Seek(currentBench);
+    }
+
+    //OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
+    override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        Vector3 dist = (animator.gameObject.transform.position - currentBench);
+        if ( dist.sqrMagnitude < 6)
+        {
+            animator.SetTrigger("sit");
+        }
+        //Debug.Log(dist.sqrMagnitude);
+    }
+
+    public override void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        animator.gameObject.transform.position = currentBench;
+        animator.gameObject.transform.Rotate(new Vector3(0, 180, 0));
+    }
+}
--- AI_Lab/Assets/_StateMachines/WanderState.cs
+++ AI_Lab/Assets/_StateMachines/WanderState.cs
@@ -1,11 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.AI;
 
 public class WanderState : StateMachineBehaviour
 {
-    NavMeshAgent agent;
     Movement movement;
 
     float t = 0.0f;
@@ -26,30 +24,15 @@ public class WanderState : StateMachineBehaviour
 
         if(t > 7.0f)
         {
-            movement.Wander();
             t = 0.0f;
-            if (Random.Range(0, 4) == 0)
+            if (Random.Range(0, 1) == 0)
             {
                 animator.SetTrigger("goBench");
             }
+            else
+            {
+                movement.Wander();
+            }
         }
     }
-
-    // OnStateExit is called when a transition ends and the state machine finishes evaluating this state
-    //override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
-    //{
-    //    
-    //}
-
-    // OnStateMove is called right after Animator.OnAnimatorMove()
-    //override public void OnStateMove(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
-    //{
-    //    // Implement code that processes and affects root motion
-    //}
-
-    // OnStateIK is called right after Animator.OnAnimatorIK()
-    //override public void OnStateIK(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
-    //{
-    //    // Implement code that sets up animation IK (inverse kinematics)
-    //}
 }
